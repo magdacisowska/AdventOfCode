d1ea3637938b45ae43b8dcec4b0e05fdcc9fd7c8
Magda <madcis@wp.pl>
2019-01-22 12:26:09 +0100

    added second part of day 3

--- AOC/day3.py
+++ AOC/day3.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 def decoder(inputs):
-    claims = np.zeros(shape=(np.size(inputs), 4))
+    claims = np.zeros(shape=(len(inputs), 4))
 
     for line in inputs:
         # pre-processing
@@ -23,22 +23,45 @@ def decoder(inputs):
 def find_duplicates(claims):
     occupied_squares = []
 
-    for index in range(claims.shape[0]):
+    for index in range(len(claims)):
 
         margin_left = int(claims[index][0])
         margin_up = int(claims[index][1])
         dim_x = int(claims[index][2])
         dim_y = int(claims[index][3])
 
-        # create pairs (x, y) of busy squares and then change them to string format 'xy'- for further easier recognition
+        # create string pairs 'x-y' of busy squares
         for y in range(dim_y):
             for x in range(dim_x):
-                occupied_squares.append(str(margin_left + x) + str(margin_up + y))
+                occupied_squares.append(str(margin_left + x) + '-' + str(margin_up + y))
 
-    # iterates through pairs 'xy', counting the number of recurrent ones
+    # iterates through pairs 'x-y', counting the number of recurrent ones
     dupes = [item for item, count in Counter(occupied_squares).items() if count > 1]
 
-    return np.size(dupes)
+    return dupes, len(dupes)
+
+
+def find_the_one(claims, dupes):
+
+    for index in range(len(claims)):
+        print(index)
+        is_interfering = False
+
+        margin_left = int(claims[index][0])
+        margin_up = int(claims[index][1])
+        dim_x = int(claims[index][2])
+        dim_y = int(claims[index][3])
+
+        for y in range(dim_y):
+            for x in range(dim_x):
+                coords = str(margin_left + x) + '-' + str(margin_up + y)
+                if coords in dupes:
+                    is_interfering = True
+                    break
+                elif y == dim_y - 1 and x == dim_x - 1 and is_interfering is False:
+                    return index + 1
+
+    return None
 
 
 if __name__ == '__main__':
@@ -46,6 +69,7 @@ if __name__ == '__main__':
     inputs = open('inputs/input3.txt').read().splitlines()
 
     claims = decoder(inputs)
-    duplicates = find_duplicates(claims)
+    dupes, num_of_dupes = find_duplicates(claims)
 
-    print('Number of duplicated (or more) square inches: {}'.format(duplicates))
+    print('Number of duplicated (or more) square inches: {}'.format(num_of_dupes))
+    print('The one not interfering has the index of '.format(find_the_one(claims, dupes)))
