22a7938e094578dcdec8d60eb59307cf2a2de26f
Magda <madcis@wp.pl>
2019-01-23 12:46:22 +0100

    day1 update

--- AOC/day1.py
+++ AOC/day1.py
@@ -2,20 +2,18 @@ import numpy as np
 
 input_data = np.loadtxt('inputs/input1.txt')
 current_freq = 0
-doubled_freq = None
+doubled_freq = 0
 freqs = []
 is_found = False
 
-while not is_found:
-    for freq in input_data:
+for freq in input_data:
 
-        freqs.append(current_freq)
-        current_freq += freq
+    freqs.append(current_freq)
+    current_freq += freq
 
-        if current_freq in freqs and not is_found:
-            doubled_freq = current_freq
-            is_found = True
-
+    if current_freq in freqs and not is_found:
+        doubled_freq = current_freq
+        is_found = True
 
 print('Final frequency: {}'.format(current_freq))
 print('{} occurred twice as first'.format(doubled_freq))
